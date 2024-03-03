c5712e10ccc22993c3e183e17b96360174bbb231
voxj <129677054+voxj@users.noreply.github.com>
2024-03-03 18:23:53 +0300

    Update os.py

--- extensions/os.py
+++ extensions/os.py
@@ -4,14 +4,16 @@ try:
   from colorama import Fore, Style
   pass
 except Exception:
+  print(f'{__name__} resulted in an error. We\'ll try to fix it.')
   os.system('python -m pip install colorama')
   os.system('pip install colorama')
+  os.system('pacman -S python-colorama')
   from colorama import Fore, Style
   pass
 blue = Fore.BLUE
 reset = Style.RESET_ALL
 log = lv()
-__version__ = "1.1"
+__version__ = "1.2"
 def initialize():
   if log == True:
     print(f'Note from {__name__}.py: The OS extension is not too good yet, but if you really insist on using it, well.. okay I guess...')
@@ -26,6 +28,12 @@ def oz():
       break
     else:
       os.system(x)
+def helpos():
+  print('Commands for OS')
+  print('help os - shows this message')
+  print('ver os - shows the version of the extension')
+  print('os - opens a shell (doesn\'t work good')
 def register_commands(commands):
   commands['os'] = oz
+  commands['help os'] = helpos
   commands['ver os'] = ver

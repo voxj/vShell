d7af9f5ded2154c196235a48d0b11833941d2e7e
voxj <129677054+voxj@users.noreply.github.com>
2024-03-22 14:26:55 +0300

    Update super.py

--- extensions/super.py
+++ extensions/super.py
@@ -2,19 +2,12 @@ import os
 from tools import retrieve
 import sys
 pyver = sys.version
-__version__ = "1.1"
+__version__ = "1.1.1"
 devMode = retrieve.dm()
 try:
     import colorama
 except Exception:
-  print(f'{__name__} resulted in an error. We\'ll try to fix it.')
-  os.system('python -m pip install colorama')
-  os.system('pip install colorama')
-  os.system('sudo pacman -S python-colorama')
-  try:
-    import colorama
-  except Exception:
-    print(f'Automatic error fixing for {__name__} hasn\'t worked. Please disable the extension or download a previous version of it.')
+  print(f'{__name__} has an error. Autofixes aren\'t applied due to their unstability.')
 blue = colorama.Fore.BLUE
 reset = colorama.Style.RESET_ALL
 def initialize():
@@ -23,18 +16,18 @@ def initialize():
   else:
     pass
 def open_shell():
-    os.system('python')
+    os.system('py')
 def verpy():
-    print(f'{blue}superPY v{__version__} | Wanted to see the shell version? Use ver pyshell!')
+    print(f'{blue}superPY v{__version__} | Wanted to see the shell version? Use ver pyshell!{reset}')
 def pyshve():
-    print(f'{blue}Python {pyver}{reset} | Wanted to see the extension version? Use ver spy!')
+    print(f'{blue}Python {pyver} | Wanted to see the extension version? Use ver spy!{reset}')
 def helppy():
     print('Commands for Python')
     print('help py (or help python) - shows this message')
     print('ver py (or ver python) - tells the version of the extension')
-    print('superPY - opens a Python shell')
+    print('spy - opens a Python shell')
 def register_commands(commands):
-    commands['superPY'] = open_shell
+    commands['spy'] = open_shell
     commands['help spy'] = helppy
     commands['help superPY'] = helppy
     commands['ver spy'] = verpy

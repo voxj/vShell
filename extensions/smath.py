292ca7dd0207a317b248030a13deb0ed08757301
voxj <129677054+voxj@users.noreply.github.com>
2024-03-22 14:26:48 +0300

    Update smath.py

--- extensions/smath.py
+++ extensions/smath.py
@@ -1,17 +1,16 @@
-from tools import retrieve
+from tools import retrieve, acls
+from main import dir
 nt = retrieve.nt()
+def cls():
+  acls.cls()
 import os
 try:
   from colorama import Fore, Style
 except Exception:
-  print(f'{__name__} resulted in an error. We\'ll try to fix it.')
-  os.system('python -m pip install colorama')
-  os.system('pip install colorama')
-  os.system('sudo pacman -S python-colorama')
-  from colorama import Fore, Style
+  print(f'{__name__} has an error. Autofixes aren\'t applied due to their unstability.')
 blue = Fore.BLUE
 reset = Style.RESET_ALL
-__version__ = "1.3.1"
+__version__ = "1.4.1"
 def ver():
   print(f"{blue}Simply, Math v{__version__}{reset}")
 def helpsmth():
@@ -28,48 +27,88 @@ def initialize():
   else:
     pass
 def mtp():
-  n1 = float(input("First number: "))
-  n2 = float(input("Second number: "))
+  cls()
+  n1 = int(input(f'{blue}{dir}{reset}> mtp '))
+  cls()
+  n2 = int(input(f'{blue}{dir}{reset}> mtp {n1} '))
+  cls()
+  print(f'{blue}{dir}{reset}> mtp {n1} {n2}')
+  cls()
   r = n1 * n2
   if r.is_integer():
     r = int(r)
-  print(r)
+  print(f'{blue}{dir}{reset}> mtp {n1} {n2}\n{blue}{reset}{r}')
 def sum():
-  n1 = float(input("First number: "))
-  n2 = float(input("Second number: "))
+  cls()
+  n1 = int(input(f'{blue}{dir}{reset}> sum '))
+  cls()
+  n2 = int(input(f'{blue}{dir}{reset}> sum {n1} '))
+  cls()
+  print(f'{blue}{dir}{reset}> sum {n1} {n2}')
+  cls()
   r = n1 + n2
   if r.is_integer():
     r = int(r)
-  print(r)
+  print(f'{blue}{dir}{reset}> sum {n1} {n2}\n{blue}{reset}{r}')
 def div():
-  n1 = float(input("First number: "))
-  n2 = float(input("Second number: "))
+  cls()
+  n1 = int(input(f'{blue}{dir}{reset}> div '))
+  cls()
+  n2 = int(input(f'{blue}{dir}{reset}> div {n1} '))
+  cls()
+  print(f'{blue}{dir}{reset}> div {n1} {n2}')
+  cls()
   r = n1 / n2
   if r.is_integer():
     r = int(r)
-  print(r)
+  print(f'{blue}{dir}{reset}> div {n1} {n2}\n{blue}{reset}{r}')
 def sub():
-  n1 = float(input("First number: "))
-  n2 = float(input("Second number: "))
-  r = n1 - n2
-  if r.is_integer():
-    r = int(r)
-  print(r)
+  cls()
+  n1 = int(input(f'{blue}{dir}{reset}> sub '))
+  cls()
+  n2 = int(input(f'{blue}{dir}{reset}> sub {n1} '))
+  cls()
+  print(f'{blue}{dir}{reset}> sub {n1} {n2}')
+  cls()
+  r = n1 ^ n2
+  print(f'{blue}{dir}{reset}> sub {n1} {n2}\n{blue}{reset}{r}')
 def pow():
-  n1 = int(input("First number: "))
-  n2 = int(input("Second number: "))
+  cls()
+  n1 = int(input(f'{blue}{dir}{reset}> pow '))
+  cls()
+  n2 = int(input(f'{blue}{dir}{reset}> pow {n1} '))
+  cls()
+  print(f'{blue}{dir}{reset}> pow {n1} {n2}')
+  cls()
   r = n1 ** n2
-  print(r)
+  print(f'{blue}{dir}{reset}> pow {n1} {n2}\n{blue}{reset}{r}')
+def exp2():
+  cls()
+  n1 = int(input(f'{blue}{dir}{reset}> exp '))
+  cls()
+  n2 = int(input(f'{blue}{dir}{reset}> exp {n1} '))
+  cls()
+  print(f'{blue}{dir}{reset}> exp {n1} {n2}')
+  cls()
+  r = n1 ** n2
+  print(f'{blue}{dir}{reset}> exp {n1} {n2}\n{blue}{reset}{r}')
 def exp():
-  r = int(input("First number: ")) ^ int(input("Second number: "))
-  print(r)
+  cls()
+  n1 = int(input(f'{blue}{dir}{reset}> xor '))
+  cls()
+  n2 = int(input(f'{blue}{dir}{reset}> xor {n1} '))
+  cls()
+  print(f'{blue}{dir}{reset}> xor {n1} {n2}')
+  cls()
+  r = n1 ^ n2
+  print(f'{blue}{dir}{reset}> xor {n1} {n2}\n{r}{reset}')
 def register_commands(commands):
   commands['sum'] = sum
   commands['mtp'] = mtp
   commands['div'] = div
   commands['sub'] = sub
   commands['pow'] = pow
-  commands['exp'] = pow
+  commands['exp'] = exp2
   commands['xor'] = exp
   commands['ver smath'] = ver
   commands['ver simplymath'] = ver
